ec5e116120544f71fa6ff1faaf1efe8910a9af2a
Efrino Wahyu Eko Pambudi <efrinowep@gmail.com>
2024-11-14 01:45:24 +0700

    add login

--- server.js
+++ server.js
@@ -15,14 +15,48 @@ db.query('SELECT 1', (err, results) => {
   }
 });
 
-// Middleware setup
 app.use(cors());
 app.use(bodyParser.json());
 
-// Example endpoint
-app.get('/', (req, res) => {
-  res.send('Agrojaya jaya jaya');
+// Rute login
+app.post('/api/login', (req, res) => {
+  const { username, password } = req.body;
+  const query = 'SELECT username, role, firstname FROM user WHERE username = ? AND password = ?';
+  db.query(query, [username, password], (err, results) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    if (results.length > 0) {
+      const user = results[0];
+      const token = jwt.sign({ username: user.username, role: user.role, firstname: user.firstname }, 'your_secret_key', { expiresIn: '24h' });
+      res.send({ message: 'Login successful', token });
+    } else {
+      res.status(401).send({ message: 'Invalid credentials' });
+    }
+  });
 });
+
+// Middleware untuk verifikasi token
+const verifyToken = (req, res, next) => {
+  const token = req.headers['authorization'];
+  if (!token) {
+    return res.status(403).send({ message: 'No token provided' });
+  }
+
+  jwt.verify(token.split(' ')[1], 'your_secret_key', (err, decoded) => {
+    if (err) {
+      return res.status(500).send({ message: 'Failed to authenticate token' });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+
+// Rute untuk mendapatkan status pengguna yang sedang login
+app.get('/api/user', verifyToken, (req, res) => {
+  res.send({ user: req.user });
+});
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
